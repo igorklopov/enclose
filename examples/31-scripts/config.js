7ab45506b8a427fc1617932ea7cad01b796d7ddb
igor <igor@klopov.com>
2015-02-07 22:57:15 +0300

    defined but never used

--- examples/31-scripts/config.js
+++ examples/31-scripts/config.js
@@ -1,8 +1,3 @@
-"use strict";
-
-var windows = process.platform === "win32";
-var wexe = windows ? ".exe" : "";
-
 module.exports = {
   scripts: "./views/**/*"
 };
--- examples/31-scripts/index.js
+++ examples/31-scripts/index.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 
+/* eslint camelcase:0 */
+
 // ./compile.js
 
 var non_literal_in_require = "./views/profile.js";
