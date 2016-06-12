42ea7c6a5e4f086fe26e143e49dcab68c17117bc
root <root@c8314f6d5276>
2016-06-12 12:26:10 +0000

    fix for scaleway

--- bin/enclose.js
+++ bin/enclose.js
@@ -82,6 +82,7 @@ function objectToArgs(argo) {
 
 function getArmArch() {
   var cpu = fs.readFileSync("/proc/cpuinfo", "utf8");
+  if (cpu.indexOf("vfpv3") >= 0) return "armv7";
   var name = cpu.split("model name")[1];
   if (name) name = name.split(":")[1];
   if (name) name = name.split("\n")[0];
