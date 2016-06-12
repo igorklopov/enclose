e9c950d48a611d089977bb6685f785d6b26743be
Igor Klopov <igor@klopov.com>
2016-06-12 15:09:45 +0300

    better armv6 detection

--- bin/enclose.js
+++ bin/enclose.js
@@ -82,9 +82,11 @@ function objectToArgs(argo) {
 
 function getArmArch() {
   var cpu = fs.readFileSync("/proc/cpuinfo", "utf8");
-  var found = cpu.match(/CPU architecture: (.?)\n/);
-  if (!found || !found[1]) return "armv6";
-  return "armv" + found[1];
+  var name = cpu.split("model name")[1];
+  if (name) name = name.split(":")[1];
+  if (name) name = name.split("\n")[0];
+  if (name && name.indexOf("ARMv7") >= 0) return "armv7";
+  return "armv6";
 }
 
 function getArch() {
